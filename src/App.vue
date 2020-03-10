2eddc728d7194e7f94b262f95cddbf74a05e8093
GlevDev <garth@glev.fr>
2020-03-10 09:28:22 +0100

    ajout API

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,20 @@
     <div id="nav">
       <main>
         <div class="search-box">
-          <input type="text" class="search-bar" placeholder="Chercher..."/>
+          <input type="text" class="search-bar" placeholder="Chercher..." v-model="query" @keypress="fetchWeather"/>
+        </div>
+        <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+          <div class="location-box">
+            <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
+            <div class="date">{{dateBuilder()}}</div>
+          </div>
+          <div class="weather-box">
+            <div class="temp">{{Math.round(weather.main.temp)}}°C</div>
+            <div class="weather">{{weather.weather[0].main}}</div>
+          </div>
         </div>
       </main>
     </div>
-    <router-view/>
   </div>
 </template>
 
@@ -16,7 +25,26 @@ export default {
   name: 'app',
   data () {
     return {
-      api_key: 'e20751b3db473472d377bcc0badc3337'
+      api_key: 'e20751b3db473472d377bcc0badc3337',
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
+    }
+  },
+  methods: {
+    fetchWeather (event) {
+      if (event.key === 'Enter') {
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`).then(res => {
+          return res.json()
+        }).then(this.setResults)
+      }
+    },
+    setResults (results) {
+      this.weather = results
+    },
+    dateBuilder () {
+      const d = new Date()
+      return d.toLocaleDateString('fr-FR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
     }
   }
 }
@@ -70,5 +98,46 @@ main {
   box-shadow: 0px 0px 16px rgba(0,0,0,0.25);
   background-color: rgba(255,255,255,0.75);
   border-radius: 16px 0px 16px 0px;
+}
+
+.location-box .location {
+  color: #FFF;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0,0,0, 0.25);
+}
+
+.location-box .date {
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #FFF;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  color: #FFF;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 </style>
