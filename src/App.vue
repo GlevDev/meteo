a0cd26525aeef2ceefff7623dfd50a5d7c443601
GlevDev <garth@glev.fr>
2020-03-11 00:00:28 +0100

    modif image de fond

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <div id="app">
+  <div id="app" :style="typeof weather.main != 'undefined' ? `background-image: url(${backgroundURL})` : ''">
     <div id="nav">
       <main>
         <div class="search-box">
-          <input type="text" class="search-bar" placeholder="Chercher..." v-model="query" @keypress="fetchWeather"/>
+          <input
+            type="text"
+            class="search-bar"
+            placeholder="Chercher..."
+            v-model="query"
+            @keypress="fetchWeather"
+          />
         </div>
         <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
           <div class="location-box">
@@ -12,7 +18,7 @@
           </div>
           <div class="weather-box">
             <div class="temp">{{Math.round(weather.main.temp)}}°C</div>
-            <div class="weather">{{weather.weather[0].main}}</div>
+            <div class="weather">{{weather.weather[0].description}}</div>
           </div>
         </div>
       </main>
@@ -34,9 +40,13 @@ export default {
   methods: {
     fetchWeather (event) {
       if (event.key === 'Enter') {
-        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`).then(res => {
-          return res.json()
-        }).then(this.setResults)
+        fetch(
+          `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}&lang=fr`
+        )
+          .then(res => {
+            return res.json()
+          })
+          .then(this.setResults)
       }
     },
     setResults (results) {
@@ -44,7 +54,17 @@ export default {
     },
     dateBuilder () {
       const d = new Date()
-      return d.toLocaleDateString('fr-FR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
+      return d.toLocaleDateString('fr-FR', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      })
+    }
+  },
+  computed: {
+    backgroundURL () {
+      return require(`./assets/${this.weather.weather[0].main}.jpg`)
     }
   }
 }
@@ -58,20 +78,28 @@ export default {
 }
 
 body {
-  font-family: 'montserrat', sans-serif;
+  font-family: "montserrat", sans-serif;
 }
 
 #app {
-  background-image: url('./assets/cold-bg.jpg');
+  background-image: url("./assets/cold-bg.jpg");
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
 }
 
+#app.warm {
+  background-image: url("./assets/warm-bg.jpg");
+}
+
 main {
   min-height: 100vh;
   padding: 25px;
-  background-image: linear-gradient(to bottom, rgba(0,0,0,0.25), rgba(0,0,0,0.75));
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
 }
 
 .search-box {
@@ -89,27 +117,27 @@ main {
   border: none;
   outline: none;
   background: none;
-  background-color: rgba(255,255,255,0.5);
+  background-color: rgba(255, 255, 255, 0.5);
   border-radius: 0px 16px 0px 16px;
   transition: 0.4s;
 }
 
 .search-box .search-bar:focus {
-  box-shadow: 0px 0px 16px rgba(0,0,0,0.25);
-  background-color: rgba(255,255,255,0.75);
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
   border-radius: 16px 0px 16px 0px;
 }
 
 .location-box .location {
-  color: #FFF;
+  color: #fff;
   font-size: 32px;
   font-weight: 500;
   text-align: center;
-  text-shadow: 1px 3px rgba(0,0,0, 0.25);
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
 }
 
 .location-box .date {
-  color: #FFF;
+  color: #fff;
   font-size: 20px;
   font-weight: 300;
   font-style: italic;
@@ -123,7 +151,7 @@ main {
 .weather-box .temp {
   display: inline-block;
   padding: 10px 25px;
-  color: #FFF;
+  color: #fff;
   font-size: 102px;
   font-weight: 900;
   text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
@@ -134,7 +162,7 @@ main {
 }
 
 .weather-box .weather {
-  color: #FFF;
+  color: #fff;
   font-size: 48px;
   font-weight: 700;
   font-style: italic;
